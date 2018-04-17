2d3fabb48ce030f37dee3f53118eb032359fe1b5
rachel <rachel.culpepper@richmond.edu>
2018-04-17 12:21:10 -0400

    added function for hashing in header and some additions to design notes

--- Project4Header.h
+++ Project4Header.h
@@ -19,22 +19,68 @@
 #define	LISTENQ		1024	/* 2nd argument to listen() */
 #define SHORT_BUFFSIZE  100     /* For messages I know are short */
 
+#define boolean int
+#define true 1
+#define false 0
+
+
+int FILE_NAME_MAX = 255;
+
+// structure that forms the beginning of each message
 struct header
 {
     char type[4];
-    int length;
-}
+    int length;  // the number of data items included in the message
+};
 
 
+// structure used for LIST requests -- provides the name of each
+// file and its hash to detect duplicate files
 struct file_name
 {
-    char filename[255];  // max of 255 chars in file name
-    unsigned char hash[32];
-}
+    char filename[ 255 ];
+    char hash[64];
+};
 
 
+// structure for messages that include files
+// each file is preceded by this structure, which
+// indicates the file's name and size (in bytes)
 struct push_file
 {
-    unsigned int size; // size in bytes of file
-    char name[255]; // file name -- max 255 chars
+    unsigned int size;
+    char name[ 255 ];
+};
+
+
+
+/* Computes and returns the SHA256 hash of the file in the current
+ * directory with name filename.
+ * Returns a pointer to a char array that represents the string of
+ * the hash. The array is 64 characters long and not null-terminated.
+ */
+char* computeHash( char* filename )
+{
+    // make enough room for command "sha256sum", quotes, and filename
+    size_t fcn_len = strlen( "sha256sum \"" );
+    size_t len = strlen( filename );
+
+    size_t command_len = fcn_len + len + 1;
+
+    // construct string for command to hash specified file
+    char command[ command_len + 1 ];
+    strncpy( command, "sha256sum \"", fcn_len );
+    strncpy( &command[ fcn_len ], filename, len );
+    command[ command_len - 1 ] = '\"';
+    command[ command_len ] = '\0';
+
+    // hash will always be 64 chars
+    char* hash_str = malloc( 64 );
+
+    // get hash of file i
+    FILE* hash_pipe = popen( command, "r" );
+    fgets( hash_str, 64, hash_pipe );
+    pclose( hash_pipe );
+
+    return hash_str;
 }
